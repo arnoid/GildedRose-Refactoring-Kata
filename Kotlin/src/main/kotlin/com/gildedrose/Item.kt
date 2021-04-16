1041177abe3127fc5e8bdd471db2104829b4823d
arnoid <restfulsaturn@gmail.ccom>
2021-04-16 13:41:26 +0200

    [ADD] business rules to javadocs

--- Kotlin/src/main/kotlin/com/gildedrose/Item.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/Item.kt
@@ -1,5 +1,10 @@
 package com.gildedrose
 
+/**
+ * @param name name
+ * @param sellIn value which denotes the number of days we have to sell the item
+ * @param quality value which denotes how valuable the item is
+ */
 open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
         return this.name + ", " + this.sellIn + ", " + this.quality
